ccba96195f09e11889e187f1f629446f293f4419
avinek <avinek.p.kavil@gmail.com>
2024-04-12 17:35:25 +0530

    Eslint and prettier setup, and removed husky.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
 const app = express();
 
-var i;
-
 app.get("/", (req, res) => {
   res.send("Running!");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-
-  
   console.log(`Server running on port ${PORT}`);
 });
